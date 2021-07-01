9ad471f24c37b2b7340aba4587a57267341c9e76
Abeo Anthony, A <anthonyabeo@gmail.com>
2021-07-01 18:16:13 +0000

    test parser using a simple input command

---
+++ parser/parser_test.go
@@ -0,0 +1,30 @@
+package parser
+
+import "testing"
+
+func TestParser(t *testing.T) {
+	input := "ls -al | grep foo > outfile"
+	tests := []Token{
+		{value: "ls", typ: IDENTIFIER},
+		{value: "-al", typ: OPTION},
+		{value: "|", typ: PIPE},
+		{value: "grep", typ: IDENTIFIER},
+		{value: "foo", typ: IDENTIFIER},
+		{value: ">", typ: GREAT},
+		{value: "outfile", typ: IDENTIFIER},
+	}
+
+	parser := NewParser(input)
+
+	for _, tok := range tests {
+		if parser.curToken.typ != tok.typ {
+			t.Errorf("Incorrect token. Got=%v, Expected=%v", parser.curToken.typ, IDENTIFIER)
+		}
+
+		if parser.curToken.value != tok.value {
+			t.Errorf("Wrong token value. Got=%v, Expected=ls", parser.curToken.value)
+		}
+
+		parser.NextToken()
+	}
+}
