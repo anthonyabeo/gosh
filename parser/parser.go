7d98b16e7758db801d78ca792c2ce837787cbf04
Abeo Anthony, A <anthonyabeo@gmail.com>
2021-07-05 23:12:48 +0000

    introduce parseCommand method

--- parser/parser.go
+++ parser/parser.go
@@ -1,5 +1,11 @@
 package parser
 
+import (
+	"os/exec"
+
+	"github.com/anthonyabeo/gosh/executor"
+)
+
 type Parser struct {
 	lex       Lexer
 	curToken  Token
@@ -19,10 +25,6 @@ func (p *Parser) NextToken() {
 	p.nextToken = p.lex.NextToken()
 }
 
-func (p *Parser) CurToken() Token {
-	return p.curToken
-}
-
 func (p *Parser) CurTokenIs(tt Token) bool {
 	return p.curToken.Typ == tt.Typ
 }
@@ -31,6 +33,29 @@ func (p *Parser) NextTokenIs(tt Token) bool {
 	return p.nextToken.Typ == tt.Typ
 }
 
-// func (p *Parser) ParseCommand() CompleteCommand {
+func (p *Parser) ParseCommand() executor.CompleteCommand {
+	cc := executor.CompleteCommand{}
 
-// }
+	for p.curToken.Typ != EOF {
+		cmd, err := p.parseCmd()
+		if err != nil {
+			cc.Commands = append(cc.Commands, cmd)
+		}
+
+		p.NextToken()
+	}
+
+	return cc
+}
+
+func (p *Parser) parseCmd() (exec.Cmd, error) {
+	cmd := exec.Cmd{Path: p.curToken.Value}
+	p.NextToken()
+
+	for p.curToken.Typ == IDENTIFIER || p.curToken.Typ == OPTION {
+		cmd.Args = append(cmd.Args, p.curToken.Value)
+		p.NextToken()
+	}
+
+	return cmd, nil
+}
