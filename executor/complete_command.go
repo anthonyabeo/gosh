25b100b602da387e9f452bcf7ac465e7cd88e6dd
Abeo Anthony, A <anthonyabeo@gmail.com>
2021-07-09 09:02:49 +0000

    implement execute method on CompleteCommand

--- executor/complete_command.go
+++ executor/complete_command.go
@@ -1,14 +1,40 @@
 package executor
 
 import (
+	"fmt"
 	"io"
+	"os"
 	"os/exec"
 )
 
 type CompleteCommand struct {
-	Stdin      io.Reader
-	Stdout     io.Writer
-	Stderr     io.Writer
-	Background bool
-	Commands   []exec.Cmd
+	Stdin        io.Reader
+	Stdout       io.Writer
+	Stderr       io.Writer
+	Background   bool
+	Commands     []*exec.Cmd
+	NumAvailCmds uint
+	NumCmds      uint
+}
+
+func NewCompleteCommand() *CompleteCommand {
+	return &CompleteCommand{
+		Stdin:      os.Stdin,
+		Stdout:     os.Stdout,
+		Stderr:     os.Stderr,
+		Background: false,
+	}
+}
+
+func (cc *CompleteCommand) Execute() {
+	for _, cmd := range cc.Commands {
+		cmdOut, err := cmd.Output()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "\nError - %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Fprintf(os.Stdout, "%s\n", string(cmdOut))
+	}
 }
